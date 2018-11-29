c08e4022a59bbd62bcf70524efe11fd6d652719d
Henrique Pegacha <A44003@alunos.isel.pt>
2018-11-29 09:59:03 +0000

    Setup inicial

---
+++ Ficha5EDA/Circulo.cpp
@@ -0,0 +1,34 @@
+#include "Circulo.h"
+
+
+
+Circulo::Circulo()
+{
+}
+
+
+void Circulo::desenhar(HWND janelaId, long cor)
+{
+	if (janelaId != NULL)
+	{
+		HDC DrawHDC = GetDC(janelaId);
+		// penstyle, width, color
+		HPEN hNPen = CreatePen(PS_SOLID, 2, cor);
+		HPEN hOPen = (HPEN)SelectObject(DrawHDC, hNPen);
+		HBRUSH hOldBrush;
+		HBRUSH hNewBrush;
+		hNewBrush = CreateSolidBrush(cor);
+		hOldBrush = (HBRUSH)SelectObject(DrawHDC, hNewBrush);
+		Ellipse(DrawHDC, centro.obterX() - raio, centro.obterY() + raio,
+			centro.obterX() + raio, centro.obterY() - raio);
+		char str[80];
+		sprintf_s(str, "%d", valor);
+		SetBkMode(DrawHDC, TRANSPARENT);
+		SetTextColor(DrawHDC, RGB(0, 0, 0));
+		SetTextAlign(DrawHDC, TA_CENTER | TA_BOTTOM | TA_BASELINE);
+		TextOut(DrawHDC, centro.obterX(), centro.obterY(), str, strlen(str));
+		DeleteObject(SelectObject(DrawHDC, hOPen));
+		DeleteObject(SelectObject(DrawHDC, hOldBrush));
+		ReleaseDC(janelaId, DrawHDC);
+	}
+}
